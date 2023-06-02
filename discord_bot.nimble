4788157455d09e49b57528e95dd14a29dfc2f749
L-Nafaryus <l.nafaryus@gmail.com>
2023-06-02 15:32:53 +0500

    initial commit

---
+++ discord_bot.nimble
@@ -0,0 +1,14 @@
+# Package
+
+version       = "0.1.0"
+author        = "L-Nafaryus"
+description   = "Discord bot with nim"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["discord_bot"]
+
+
+# Dependencies
+
+requires "nim >= 1.6.8"
+requires "dimscord"
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "my project description";
+
+  inputs = {
+  	nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	flake-utils.url = "github:numtide/flake-utils";
+};
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let 
+		pkgs = import nixpkgs { inherit system overlays }; 
+	in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
